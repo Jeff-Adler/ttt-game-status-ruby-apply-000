256a667052ae63a720f0fa20dd0f8c79e459954f
Jeff Adler <jeffm.adler@gmail.com>
2020-06-01 18:51:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,7 +59,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if (won?(board) == true || full?(board) == true)
+  if (won?(board) == true || draw?(board) == true)
     return true
   else
     return false
